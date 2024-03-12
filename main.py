9b715b71d3c2454dfa4972f22fb80004da1dfe1a
mikelchaparro <32418959-mikelchaparro@users.noreply.replit.com>
2024-03-12 18:55:22 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,35 @@
+# 1
+
+string_one = "Hasta la vista"
+number_one = 3
+list_one = [1,2,3,4,5]
+boolean_one = True
+
+# 2
+string_two = string_one[0:3]
+
+# 3
+first_element = list_one[0]
+
+# 4
+number_two = number_one + 10
+
+# 5
+last_element = list_one[-1]
+
+# 6
+names = "harry,alex,susie,jared,gail,conner"
+list_of_names = names.split(",")
+
+# 7
+first_word = string_one[0:5].upper()
+upper_string = first_word + string_one[5:]
+
+# 8
+interpolation_string = f"Your package will arrive in {number_one} days"
+
+# 9
+print("Hello World")
+
+
+
